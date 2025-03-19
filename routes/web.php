142d582aae5cf2487e70bc104f189586f3a15c3b
kezocode <strawberrykisses65@gmail.com>
2025-03-19 19:30:41 +0900

    created routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,40 @@
 <?php
 
+use App\Http\Controllers\ContactFormController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+
+// Route::view('/', 'index');
+
+// Route::view('/japan/places', 'places', ['name' => 'places'])->name('places');
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/japan/places', function () {
+    return view('japan.places');
+})->name('places');
+
+Route::get('/japan/food', function () {
+    return view('japan.food');
+})->name('food');
+
+Route::get('/japan/fun', function () {
+    return view('japan.fun');
+})->name('fun');
+
+Route::get('/japan/history', function () {
+    return view('japan.history');
+})->name('history');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::post('/contact', [ContactFormController::class, 'submit'])->name('contact.submit');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
