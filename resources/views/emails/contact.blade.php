86a4e301c06f147b82832affe185486ea0f9f07a
kezocode <strawberrykisses65@gmail.com>
2025-03-19 19:29:02 +0900

    made contact page

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contact Form Submission</title>
+</head>
+
+<body>
+    <p>Email: {{ $details['email'] }}</p>
+    <p>Subject: {{ $details['subject'] }}</p>
+    <p>Message: {{ $details['message'] }}</p>
+</body>
+
+</html>
