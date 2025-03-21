40b2586effef6a330ffe50debb01542f9fafab08
kezocode <strawberrykisses65@gmail.com>
2025-03-21 15:58:55 +0900

    made header fixed to top

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,8 +12,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
 </head>
 
-<body class="h-full">
-    <nav class="bg-red-600">
+<body class="h-full pt-16">
+    <nav class="fixed left-0 right-0 top-0 z-50 w-full bg-red-600 shadow-md">
         <div class="mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center">
 
@@ -28,16 +28,20 @@
                     <a class="font-[Oswald] text-2xl text-white hover:text-black" href="{{ route('fun') }}">Fun</a>
                     <a class="font-[Oswald] text-2xl text-white hover:text-black"
                         href="{{ route('history') }}">History</a>
+
+                </div>
+
+                <div class="flex w-1/3 justify-end">
                     <a class="font-[Oswald] text-2xl text-white hover:text-black"
                         href="{{ route('contact') }}">Contact</a>
                 </div>
 
-                <div class="w-1/3"></div>
             </div>
         </div>
     </nav>
 
     {{ $slot }}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.0/flowbite.min.js"></script>
 </body>
 
 </html>
