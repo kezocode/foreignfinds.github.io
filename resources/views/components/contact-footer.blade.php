b04372a2be0dc597bc3cb8cb2068806cd47ee964
kezocode <strawberrykisses65@gmail.com>
2025-03-24 16:53:14 +0900

    made contact footer component

---
+++ resources/views/components/contact-footer.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-white py-12 text-center shadow-sm">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <h1 class="font-[Montserrat] text-3xl font-bold text-gray-900">
+            Want to learn something about Japan that we haven't written? Contact us!
+        </h1>
+    </div>
+    <div class="mt-12">
+        <a class="rounded-lg bg-red-600 px-10 py-4 font-[Montserrat] text-2xl text-white transition-transform duration-300 hover:scale-105 hover:bg-red-700"
+            href="{{ route('contact') }}">
+            Contact
+        </a>
+    </div>
+</div>
