ff2dd7d7ce337f6baa1877dba0e71e4463159b9e
kezocode <strawberrykisses65@gmail.com>
2025-03-24 16:53:00 +0900

    made footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mb-6 w-full bg-gray-800 py-4 text-center font-[playfair-display] text-white">
+    <small>&#169; <span id="year"></span> Foreign Finds. All Rights Reserved.</small>
+</footer>
