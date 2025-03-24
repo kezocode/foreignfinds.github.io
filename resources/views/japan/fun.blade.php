48c88068d85523144de36a861892679e0e1b26dc
kezocode <strawberrykisses65@gmail.com>
2025-03-24 16:54:08 +0900

    changed fonts

--- resources/views/japan/fun.blade.php
+++ resources/views/japan/fun.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <header class="bg-white shadow-sm">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-center font-[Oswald] text-3xl font-bold tracking-tight text-gray-900">What to do?</h1>
+            <h1 class="text-center font-[Montserrat] text-3xl font-bold tracking-tight text-gray-900">What to do?</h1>
         </div>
     </header>
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p class="text-center font-[Oswald] text-2xl tracking-tight text-gray-900">
+            <p class="text-center font-[Montserrat] text-2xl tracking-tight text-gray-900">
                 Whether you want to go to amusement parks, paragliding by Mt. Fuji, or a cafe where maids tend to your
                 every need, Japan has it all!
             </p>
@@ -20,12 +20,12 @@
                     <img class="h-72 w-72 rounded-lg object-cover shadow-lg" src="../img/disneysea.png"
                         alt="Tokyo DisneySea">
                     <span
-                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-5xl font-semibold text-white">
+                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 font-[Montserrat] text-5xl font-semibold text-white">
                         DisneySea
                     </span>
                 </div>
                 <div class="max-w-xl text-center sm:text-left">
-                    <p class="mb-4 text-lg text-gray-700">
+                    <p class="mb-4 font-[playfair-display] text-xl text-gray-700">
                         Tokyo DisneySea is a fantasy theme park in Tokyo Disney Resort that is unique to Japan. Inspired
                         by the myths and legends of the sea, Tokyo DisneySea is made up of eight themed ports of call:
                         Mediterranean Harbor, Mysterious Island, Mermaid Lagoon, Arabian Coast, Lost River Delta, Port
@@ -43,12 +43,12 @@
                 <div class="relative mx-4 inline-block">
                     <img class="h-72 w-72 rounded-lg object-cover shadow-lg" src="../img/fujiq.png" alt="Fuji-Q">
                     <span
-                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-5xl font-semibold text-white">
+                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 font-[Montserrat] text-5xl font-semibold text-white">
                         Fuji-Q
                     </span>
                 </div>
                 <div class="max-w-xl text-center sm:text-left">
-                    <p class="mb-4 text-lg text-gray-700">
+                    <p class="mb-4 font-[playfair-display] text-xl text-gray-700">
                         Fuji-Q Highland (富士急ハイランド) is one of Japan's most popular amusement parks, located in the Fuji
                         Five Lake region at the foot of Mount Fuji. It is best known for its thrilling, record-breaking
                         roller coasters and elaborate, anime- themed rides and attractions. The park strives to be at
@@ -64,12 +64,12 @@
                     <img class="h-72 w-72 rounded-lg object-cover shadow-lg" src="../img/paragliding.png"
                         alt="Paragliding">
                     <span
-                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-5xl font-semibold text-white">
+                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 font-[Montserrat] text-5xl font-semibold text-white">
                         Paragliding
                     </span>
                 </div>
                 <div class="max-w-xl text-center sm:text-left">
-                    <p class="mb-4 text-lg text-gray-700">
+                    <p class="mb-4 font-[playfair-display] text-xl text-gray-700">
                         Mt. Fuji is perhaps one of the most iconic symbols of Japan. It has incredible spiritual,
                         natural, and cultural significance, and is a UNESCO Heritage Site. Many tourists flock from
                         around Japan and abroad in hopes of climbing Japan’s highest mountain during the summer months.
@@ -84,12 +84,12 @@
                 <div class="relative mx-4 inline-block">
                     <img class="h-72 w-72 rounded-lg object-cover shadow-lg" src="../img/maidcafe.png" alt="Maid Cafe">
                     <span
-                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-5xl font-semibold text-white">
+                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 font-[Montserrat] text-5xl font-semibold text-white">
                         Maid Cafe
                     </span>
                 </div>
                 <div class="max-w-xl text-center sm:text-left">
-                    <p class="mb-4 text-lg text-gray-700">
+                    <p class="mb-4 font-[playfair-display] text-xl text-gray-700">
                         At a maid cafe, service goes beyond delivering drinks and desserts. Maids often perform small
                         gestures or “spells” to “enhance” your meal(many of which were born in at-home cafe) and engage
                         you in a way that feels personal and fun. The goal is to create an atmosphere of warmth, where
@@ -104,12 +104,12 @@
                 <div class="relative mx-4 inline-block">
                     <img class="h-72 w-72 rounded-lg object-cover shadow-lg" src="../img/festivals.png" alt="Festival">
                     <span
-                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 text-5xl font-semibold text-white">
+                        class="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50 font-[Montserrat] text-5xl font-semibold text-white">
                         Festival
                     </span>
                 </div>
                 <div class="max-w-xl text-center sm:text-left">
-                    <p class="mb-4 text-lg text-gray-700">
+                    <p class="mb-4 font-[playfair-display] text-xl text-gray-700">
                         There are countless local festivals (祭り, matsuri) in Japan because almost every shrine
                         celebrates one of its own. Most festivals are held annually and celebrate the shrine's deity or
                         a seasonal or historical event.
@@ -123,15 +123,13 @@
             <br>
         </div>
     </main>
-    <footer class="bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-center font-[Oswald] text-3xl font-bold tracking-tight text-gray-900">And many more!</h1>
-        </div>
-    </footer>
+
     <div class="mx-auto mb-6 max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p class="text-center font-[Oswald] text-2xl tracking-tight text-gray-900">
+        <p class="text-center font-[Montserrat] text-2xl tracking-tight text-gray-900">
             With an endless amount of exciting places to go, you'll never get bored in Japan!
         </p>
     </div>
-    {{-- add more space at bottom --}}
+
+    <x-contact-footer>
+    </x-contact-footer>
 </x-layout>
